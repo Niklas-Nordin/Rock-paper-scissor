4358e491d050ad45fd2906ba47250f676fcb8f4e
Niklas-Nordin <niklas.nordin@chasacademy.se>
2024-10-29 18:57:47 +0100

    Lägger till funktion där datorn slumpar ut ett av de tre värderna, när en knapp trycks.

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ const stenKnapp = document.getElementById("sten");
 const saxKnapp = document.getElementById("sax");
 const paseKnapp = document.getElementById("pase");
 
+const computerAlternative = ["Sten!", "Sax!", "Påse!"];
+
 
 stenKnapp.addEventListener("click", stenen);
 function stenen() {
     document.getElementById("myMove").innerHTML = "Sten!";
+    let computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+    console.log(computerChoice);
 }
 
 
 saxKnapp.addEventListener("click", saxen);
 function saxen() {
    document.getElementById("myMove").innerHTML = "Sax!"; 
+   let computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+    console.log(computerChoice);
 }
     
+
 paseKnapp.addEventListener("click", pasen);
 function pasen() {
     document.getElementById("myMove").innerHTML = "Påse!";
+    let computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+    console.log(computerChoice);
 }
 
 // Datorns val drag - randomizar mellan de olika alternativen.
+
+
