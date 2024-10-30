b919a485adc3796c2d15c1c54d9273d0ca4f27a3
Niklas-Nordin <niklas.nordin@chasacademy.se>
2024-10-30 14:31:15 +0100

    Lägger till en if else statement så man ser vem vinnaren är

--- script.js
+++ script.js
@@ -6,40 +6,75 @@ const paseKnapp = document.getElementById("pase");
 
 const computerAlternative = ["Sten!", "Sax!", "Påse!"];
 let myChoice;
-
+let computerChoice;
+let Winner;
+const loss = "Du förlorade!";
+const win = "Du vann!";
+const tie = "Ingen vann, ni valde samma!";
 // Datorns val av drag - randomizar mellan de olika alternativen - math.random raden.
 
 
 stenKnapp.addEventListener("click", stenen);
 function stenen() {
     myChoice = document.getElementById("myMove").innerHTML = "Sten!";
-    let computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+    computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+    cpuMove = document.getElementById("computersMove").innerHTML = computerChoice;
     console.log("Mitt val: " + myChoice);
     console.log("Datorns val: " + computerChoice);
+
+    if (myChoice == "Sten!" && computerChoice == "Sax!") {
+        document.getElementById("winner").innerHTML = win;
+        console.log(win);
+    } else if (myChoice == "Sten!" && computerChoice == "Påse!") {
+        document.getElementById("winner").innerHTML = loss;
+        console.log(loss);
+    } else {
+        document.getElementById("winner").innerHTML = tie;
+        console.log(tie);
+    }
 }
 
 
 saxKnapp.addEventListener("click", saxen);
 function saxen() {
    myChoice = document.getElementById("myMove").innerHTML = "Sax!"; 
-   let computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+   computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+   cpuMove = document.getElementById("computersMove").innerHTML = computerChoice;
    console.log("Mitt val: " + myChoice); 
    console.log("Datorns val: " + computerChoice);
+
+   if (myChoice == "Sax!" && computerChoice == "Påse!") {
+    document.getElementById("winner").innerHTML = win;
+    console.log(win);
+} else if (myChoice == "Sax!" && computerChoice == "Sten!") {
+    document.getElementById("winner").innerHTML = loss;
+    console.log(loss);
+} else {
+    document.getElementById("winner").innerHTML = tie;
+    console.log(tie);
+}
 }
     
 
 paseKnapp.addEventListener("click", pasen);
 function pasen() {
     myChoice = document.getElementById("myMove").innerHTML = "Påse!";
-    let computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+    computerChoice = computerAlternative[Math.floor(Math.random() * computerAlternative.length)];
+    cpuMove = document.getElementById("computersMove").innerHTML = computerChoice;
     console.log("Mitt val: " + myChoice);
     console.log("Datorns val: " + computerChoice);
+
+    if (myChoice == "Påse!" && computerChoice == "Sten!") {
+        document.getElementById("winner").innerHTML = win;
+        console.log(win);
+    } else if (myChoice == "Påse!" && computerChoice == "Sax!") {
+        document.getElementById("winner").innerHTML = loss;
+        console.log(loss);
+    } else {
+        document.getElementById("winner").innerHTML = tie;
+        console.log(tie);
+    }
 }
 
 
 // funktion som utger vinnaren...
-
-
-// let whoIsTheWinner = function winner() {
-//     if ()
-// }
